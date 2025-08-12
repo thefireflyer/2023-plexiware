eca1f64c66e1341a367c0715ff25d5ebd6d0e2d0
thefireflyer <theflyingfire@users.noreply.github.com>
2025-08-11 17:41:38 -0700

    renaming

--- README.md
+++ README.md
@@ -1,38 +1,24 @@
-# create-svelte
+![Tests]() ![Demo CD/CI]()
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+**Plexiware**
+- **[Read-only Demo]()**
+- **[Documentation]()**
+- [AGPLv3 License]()
 
-## Creating a project
+**Features:**
+- Calendar and scheduling system
+- Photo manager
+- Notes
+- File system
+- Contact system
+- Theming system
 
-If you're seeing this, you've probably already done this step. Congrats!
+**Dependencies:**
+- npm
 
-```bash
-# create a new project in the current directory
-npm create svelte@latest
+**Development:**
+- `npm install`
+- `npm run dev`
 
-# create a new project in my-app
-npm create svelte@latest my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+**Deployment:**
+- ...
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,44 @@
 <script lang="ts">
-    const links = [
-        {
-            url: "/account",
-            name: "Continue",
-            icon: "account_circle",
-        },
-        {
-            url: "/docs",
-            name: "Documentation",
-            icon: "description",
-        },
-        {
-            url: "/source",
-            name: "Source Code",
-            icon: "code",
-        },
-        {
-            url: "http://thefireflyer.me/homeportal/",
-            name: "Other apps",
-            icon: "apps",
-        },
-    ]
+	const links = [
+		{
+			url: '/account',
+			name: 'Continue',
+			icon: 'account_circle'
+		},
+		{
+			url: '/docs',
+			name: 'Documentation',
+			icon: 'description'
+		},
+		{
+			url: '/source',
+			name: 'Source Code',
+			icon: 'code'
+		},
+		{
+			url: 'http://thefireflyer.vercel.app/',
+			name: 'Other apps',
+			icon: 'apps'
+		}
+	];
 </script>
 
-<div class="grow p-3
+<div
+	class="grow p-3
 flex flex-col
-items-center gap-3">
-    <h1 class="text-4xl">OpenCloud</h1>
-    <h2 class="text-3xl text-neutral-700 dark:text-neutral-300">Your Online Garden</h2>
-    {#each links as link}
-    <a href={link.url} class="w-80
+items-center gap-3"
+>
+	<h1 class="text-4xl">Plexiware</h1>
+	<h2 class="text-3xl text-neutral-700 dark:text-neutral-300">Your Online Garden</h2>
+	{#each links as link}
+		<a
+			href={link.url}
+			class="w-80
     border rounded-full border-neutral-500 hover:bg-neutral-200 hover:dark:bg-neutral-800
-    p-1 px-2 flex items-stretch gap-2">
-        <span class="material-icons-outlined text-3xl">{link.icon}</span>
-        <span class="text-2xl">{link.name}</span>
-    </a>
-    {/each}
+    p-1 px-2 flex items-stretch gap-2"
+		>
+			<span class="material-icons-outlined text-3xl">{link.icon}</span>
+			<span class="text-2xl">{link.name}</span>
+		</a>
+	{/each}
 </div>
